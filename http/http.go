1e4dcaba700afec43c284563d8fb2c4afd2b3309
caio86 <caioluiz86@gmail.com>
2025-06-03 17:04:46 -0300

    http: create http error helper functions

---
+++ http/http.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+
+	talenthub "github.com/caio86/talentHub"
+)
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func Error(w http.ResponseWriter, r *http.Request, err error) {
+	code, message := talenthub.ErrorCode(err), talenthub.ErrorMessage(err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(ErrorStatusCode(code))
+	json.NewEncoder(w).Encode(&ErrorResponse{Error: message})
+}
+
+var codes = map[string]int{
+	talenthub.EINVALID:  http.StatusBadRequest,
+	talenthub.EINTERNAL: http.StatusInternalServerError,
+}
+
+func ErrorStatusCode(code string) int {
+	if v, ok := codes[code]; ok {
+		return v
+	}
+	return http.StatusInternalServerError
+}
+
+func FromErrorStatusCode(code int) string {
+	for k, v := range codes {
+		if v == code {
+			return k
+		}
+	}
+	return talenthub.EINTERNAL
+}
