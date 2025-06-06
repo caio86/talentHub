c9174a2f8717d5714c7ee372a6ba2c88fd217ac4
caio86 <caioluiz86@gmail.com>
2025-06-06 16:47:31 -0300

    main: create helper function to get env vars

--- cmd/talentHub/main.go
+++ cmd/talentHub/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/caio86/talentHub/http"
 	"github.com/caio86/talentHub/postgres"
-	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	port := 8080
 	svr := http.NewServer()
 
@@ -18,28 +22,18 @@ func main() {
 	svr.Addr = fmt.Sprintf(":%d", port)
 
 	// Connecting to DB
-	host := "localhost"
-	dbPort := "5432"
-	user := "postgres"
-	dbname := "talentHub"
-	password := "password"
-
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		host,
-		dbPort,
-		user,
-		dbname,
-		password,
-	)
-
-	conn, err := pgx.Connect(context.Background(), dsn)
+	dbConfig, err := loadDBVars()
 	if err != nil {
-		log.Fatalf("failed to connect to db: %v", err)
+		log.Fatal(err)
+	}
+	db := postgres.NewDB(dbConfig)
+	if err := db.Connect(); err != nil {
+		log.Fatal(err)
 	}
 
 	// Setting services
-	candidatosService := postgres.NewCandidatoService(conn)
-	vagaService := postgres.NewVagaService(conn)
+	candidatosService := postgres.NewCandidatoService(db)
+	vagaService := postgres.NewVagaService(db)
 
 	svr.CandidatoService = candidatosService
 	svr.VagaService = vagaService
@@ -48,4 +42,76 @@ func main() {
 	if err := svr.Open(); err != nil {
 		panic(err)
 	}
+}
+
+func loadPassword() (string, error) {
+	password, ok := os.LookupEnv("DB_PASSWORD")
+	if ok {
+		return password, nil
+	}
+
+	passwordFile, ok := os.LookupEnv("DB_PASSWORD_FILE")
+	if !ok {
+		return "", fmt.Errorf("DB_PASSWORD or DB_PASSWORD_FILE env vars not set")
+	}
+
+	data, err := os.ReadFile(passwordFile)
+	if err != nil {
+		return "", fmt.Errorf("failed to read password file: %w", err)
+	}
+
+	return strings.TrimSpace(string(data)), nil
+}
+
+func loadDBVars() (*postgres.DBConfig, error) {
+	username, ok := os.LookupEnv("DB_USER")
+	if !ok {
+		return nil, fmt.Errorf("DB_USER env var not set")
+	}
+
+	password, err := loadPassword()
+	if err != nil {
+		return nil, fmt.Errorf("loading password: %w", err)
+	}
+
+	host, ok := os.LookupEnv("DB_HOST")
+	if !ok {
+		return nil, fmt.Errorf("DB_HOST env var not set")
+	}
+
+	portStr, ok := os.LookupEnv("DB_PORT")
+	if !ok {
+		return nil, fmt.Errorf("DB_PORT env var not set")
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert port to int: %w", err)
+	}
+
+	dbname, ok := os.LookupEnv("DB_NAME")
+	if !ok {
+		return nil, fmt.Errorf("DB_NAME env var not set")
+	}
+
+	sslmode, ok := os.LookupEnv("DB_SSLMODE")
+	if !ok {
+		return nil, fmt.Errorf("DB_SSLMODE env var not set")
+	}
+
+	config := &postgres.DBConfig{
+		User:     username,
+		Password: password,
+		Host:     host,
+		Port:     uint16(port),
+		DBName:   dbname,
+		SSLMode:  sslmode,
+	}
+
+	err = config.Validate()
+	if err != nil {
+		return nil, fmt.Errorf("invalid config: %w", err)
+	}
+
+	return config, nil
 }
